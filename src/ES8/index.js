1754bab40d468633903907ec62b811c614913d97
Genaro Almaraz <jg08950@gmail.com>
2020-07-21 19:05:37 -0500

    ES8: async/await

--- src/ES8/index.js
+++ src/ES8/index.js
@@ -25,3 +25,38 @@ console.log(arr.length)
 const phrase = 'Genaro'
 console.log(phrase.padStart(9, 'Hi '))
 console.log(phrase.padEnd(12, ' -----'))
+
+
+/*
+    3. Async/Await
+*/
+
+const promise = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            setTimeout(() => resolve('Cool!'), 3000)
+        } else {
+            reject(new Error('Ups!'))
+        }
+    })
+}
+
+const callPromise = async () => {
+    const response = await promise()
+
+    console.log(response)
+}
+
+callPromise()
+
+const callPromiseProperly = async () => {
+    try {
+        const response = await promise()
+
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+callPromiseProperly()
